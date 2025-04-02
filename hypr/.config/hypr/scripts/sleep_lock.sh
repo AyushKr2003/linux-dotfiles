0cdd698aa9cdb9f74e3735e48df15b0edff2bbb1
Ayush Kumar Singh <ayushkrsngh2003@gmail.com>
2025-04-02 16:00:41 +0530

    nwg-drawer added

--- hypr/.config/hypr/scripts/sleep_lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# swayidle -w \
-#         timeout 10 "hyprlock" \  # Lock the screen after 10 seconds of inactivity
-#             resume '' \  
-#         timeout 120 'hyprctl dispatch dpms off' \  # Turn off the screen after 120 seconds
-#             resume 'hyprctl dispatch dpms on' \  # Turn on the screen when resumed
-#         timeout 600 'systemctl suspend'  # Suspend the system after 600 seconds
